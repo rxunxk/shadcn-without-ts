cc8633795e7219524792006070a71795f24835be
Raunak Pandey <37786416+rxunxk@users.noreply.github.com>
2023-08-14 13:27:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+//import react from "@vitejs/plugin-react-swc"; *for swc install
 const __dirname = path.dirname("./src");
 
 // https://vitejs.dev/config/
